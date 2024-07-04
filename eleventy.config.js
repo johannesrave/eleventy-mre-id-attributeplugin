be6ffe1957986e27d4364d4e4927b9f3ae11da9b
johannesrave <johannes.rave@gmail.com>
2024-07-05 00:37:21 +0200

    first okay looking composition, with a nice vertical separator

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import eleventyWebcPlugin from "@11ty/eleventy-plugin-webc";
 
 export default async function (eleventyConfig) {
 	// eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	eleventyConfig.addPassthroughCopy("src/static/**/*.css");
 	eleventyConfig.addPlugin(eleventyWebcPlugin, {
 		components: "src/_includes/_components/*.webc",
 	});
