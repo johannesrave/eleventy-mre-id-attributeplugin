efb9bdc0e243458547c17db62ebdfe3f559f7d9e
johannesrave <johannes.rave@gmail.com>
2024-07-10 14:52:09 +0200

    refactored helper methods in eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,22 +6,8 @@ export default async function (eleventyConfig) {
 	// eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPassthroughCopy("src/static/**/*.css");
 
-	eleventyConfig.addJavaScriptFunction("getToc", (content) => {
-		const dom = JSDOM.fragment(content);
-		const headings = dom.querySelectorAll("h1, h2");
-
-		console.log(headings.length);
-		console.log(typeof headings);
-
-		return Array.from(headings)
-			.flatMap((h) => {
-				const id = h.id;
-				const title = h.textContent;
-				return `<li><a href="#${id}">${title}</a></li>`;
-				//
-			})
-			.join("");
-	});
+	eleventyConfig.addJavaScriptFunction("getToc", getToc);
+	eleventyConfig.addJavaScriptFunction("addIds", addIds);
 
 	eleventyConfig.addPlugin(eleventyWebcPlugin, {
 		components: "src/_includes/_components/*.webc",
@@ -30,4 +16,36 @@ export default async function (eleventyConfig) {
 		dir: { input: "src", output: "build", layouts: "_layouts" },
 		templateFormats: ["md", "webc"],
 	};
+}
+
+function getToc(content) {
+	const dom = JSDOM.fragment(content);
+	const headings = dom.querySelectorAll("h1, h2");
+
+	return Array.from(headings)
+		.flatMap((h) => {
+			const id = h.id;
+			const title = h.textContent;
+			return `<li><a href="#${id}">${title}</a></li>`;
+			//
+		})
+		.join("");
+}
+
+function addIds(content, elementTypes = ["h1", "h2"]) {
+	const dom = new JSDOM(content).window.document.body;
+	const headings = Array.from(dom.querySelectorAll("h1, h2"));
+	headings.forEach((h) => {
+		console.log(h.textContent);
+		h.id = toKebabCase(h.textContent);
+	});
+	console.log(dom.innerHTML);
+
+	return dom.innerHTML;
+}
+
+function toKebabCase(s) {
+	return encodeURIComponent(
+		String(s).trim().toLowerCase().replace(/\s+/g, "-"),
+	);
 }
